5aa9b0032cb5c70258db0f0059740ad435021c4d
Rick Belson <belsonrj@aol.com>
2020-08-19 19:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
+}
+
+let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+let bigFluffyDog2 = new Dog("Woody", "labrador");
+
+bigFluffyDog1;
+bigFluffyDog2;
